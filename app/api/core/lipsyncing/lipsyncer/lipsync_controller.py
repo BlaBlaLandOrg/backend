7bd5d5c49fd7ac5b51272489a04a07ec68708ae3
grumpyp <info@gerryontour.de>
2023-07-30 15:43:09 +0200

    pls work now

--- app/api/core/lipsyncing/lipsyncer/lipsync_controller.py
+++ app/api/core/lipsyncing/lipsyncer/lipsync_controller.py
@@ -16,7 +16,7 @@ def create_lip_sync_file(audio_path, text: str):
     with open('output.txt') as f:
         json_data = json.load(f)
         print(json_data)
-        return json_data["mouthCues"]
+        return json.dumps(json_data["mouthCues"])
 
 
 
