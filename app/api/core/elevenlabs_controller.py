41373423ac8bd6fb0e2e57291c48289c60eb10bb
grumpyp <info@gerryontour.de>
2023-07-30 15:50:56 +0200

    fix now and ever

--- app/api/core/elevenlabs_controller.py
+++ app/api/core/elevenlabs_controller.py
@@ -47,11 +47,11 @@ class ElevenlabsController:
             f.write(audio)
         # just for the returntype :D
 
+        wav_file = file_id
         if lip_sync:
             wav_file = ElevenlabsController.convert_mp3_to_wav(file_id)
             lipsync = create_lip_sync_file(wav_file, text)
         audio_base64 = base64.b64encode(audio).decode()
-
         return Recording(path=wav_file, model=model, bytes=audio_base64, lipsync=lipsync)
 
     @staticmethod
